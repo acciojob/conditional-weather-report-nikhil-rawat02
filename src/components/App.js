e4989726d990767b92149566a3f840a08f8e1fce
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-05-25 18:53:32 +0530

    sol updated

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,14 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const weather = {
+    temperature: 19, 
+    conditions: "Sunny"
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p style={weather.temperature> 20?{backgroundColor:'red'}:{backgroundColor:'blue'}}>temperature {weather.temperature} and condition {weather.conditions}</p>
+
     </div>
   )
 }
